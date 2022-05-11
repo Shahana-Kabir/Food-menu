3fc192d6c0550f5af3bd2ca293d7adaaf4f807dd
Shahana Kabir <shahana.kabir@gmail.com>
2022-05-10 19:10:18 -0700

    Updates

---
+++ precache-manifest.9f3678a67e6608a44ba94c7a0a2dc1a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627ddfa2b5d7f39a45caf9da95b077e7",
+    "url": "/Menu/index.html"
+  },
+  {
+    "revision": "3d09a59b6fa5fe6816ef",
+    "url": "/Menu/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/Menu/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Menu/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d09a59b6fa5fe6816ef",
+    "url": "/Menu/static/js/main.8a665384.chunk.js"
+  },
+  {
+    "revision": "deca15953f4372325539",
+    "url": "/Menu/static/js/runtime-main.52917398.js"
+  }
+]);
